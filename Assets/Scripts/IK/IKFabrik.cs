929ec60336ac39ec7f48cfd3aef4728ade262cea
Xueer Zhu <sherryxueerzhu@gmail.com>
2023-11-04 19:12:31 -0700

    Move IK scripts to its own folder

--- Assets/Scripts/IK/IKFabrik.cs
+++ Assets/Scripts/IK/IKFabrik.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEditor;
-using System.Linq;
+﻿using UnityEngine;
 
 public class IKFabrik : MonoBehaviour
 {
