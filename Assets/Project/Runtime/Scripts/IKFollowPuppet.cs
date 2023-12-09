3f8d2dad9678140a7039682bf4eac3984b02469b
Xueer Zhu <sherryxueerzhu@gmail.com>
2023-12-08 17:40:05 -0800

    Fix FixTransform conflicting with IKFollowPuppet race condition

--- Assets/Project/Runtime/Scripts/IKFollowPuppet.cs
+++ Assets/Project/Runtime/Scripts/IKFollowPuppet.cs
@@ -6,17 +6,10 @@ using UnityEngine.Serialization;
 public class IKFollowPuppet : MonoBehaviour
 {
     public Transform PuppetTransform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         transform.position = PuppetTransform.position;
         transform.rotation = PuppetTransform.rotation;
-        transform.localScale = PuppetTransform.localScale;
     }
 }
